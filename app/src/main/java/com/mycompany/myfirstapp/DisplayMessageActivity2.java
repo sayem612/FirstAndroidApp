805e04f873f3c1693cd95d705c676f0cdee79b33
sayem612 <sayemedu@yahoo.com>
2015-12-06 00:10:27 -0800

    Initial commit

---
+++ app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity2.java
@@ -0,0 +1,31 @@
+package com.mycompany.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayMessageActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+
+}
